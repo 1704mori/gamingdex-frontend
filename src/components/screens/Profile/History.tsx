45d78812ff90e46ebce8143c4ae0c141690e8c06
mori1704 <1704mori@gmail.com>
2023-10-01 14:27:14 -0300

    profile(history): modify days/month loop and add tooltip

--- src/components/screens/Profile/History.tsx
+++ src/components/screens/Profile/History.tsx
@@ -1,3 +1,4 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { classes } from '@/lib/helpers/common';
 import { useMemo } from 'react'
 
@@ -10,12 +11,6 @@ type Props = {
 
 export default function History({ data }: Props) {
   const buildDayColor = (value: number) => {
-    // if (value == 0) return 'bg-accent2'
-    // if (value < 5) return 'bg-[#1890FF]'
-    // if (value < 10) return 'bg-[#2F9BFF]'
-    // if (value < 15) return 'bg-[#46A6FF]'
-    // if (value < 20) return 'bg-[#5DB1FF]'
-
     if (value == 0) return 'bg-accent2'
 
     if (value > 0 && value < 5) return "bg-[#5DB1FF]"
@@ -28,35 +23,51 @@ export default function History({ data }: Props) {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
   const currentMonth = currentDate.getMonth() + 1;
+  const today = currentDate.getDate();
 
   const startMonth = currentMonth - 5;
   const adjustedStartMonth = startMonth <= 0 ? 12 + startMonth : startMonth;
 
+
   const buildDays = useMemo(() => {
     const squares = [];
+
     for (let month = adjustedStartMonth; month <= currentMonth; month++) {
       const daysInMonth = new Date(currentYear, month, 0).getDate();
 
-      for (let day = 1; day <= daysInMonth; day++) {
+      const endDay = month === currentMonth ? today : daysInMonth;
+
+      for (let day = 1; day <= endDay; day++) {
         const dateString = `${currentYear}-${month.toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
         const dayData = data.find((item) => item.day === dateString);
-        const value = dayData ? dayData.value : 0; // Default value to 0 if data is not found.  
+        const value = dayData ? dayData.value : 0; // Default value to 0 if data is not found.
         const backgroundColor = buildDayColor(value);
 
         squares.push(
-          <div
-            key={day}
-            className={classes(
-              "w-3 h-3 rounded-sm",
-              backgroundColor
-            )}
-          />
+          <TooltipProvider>
+            <Tooltip delayDuration={50}>
+              <TooltipTrigger asChild>
+                <div
+                  key={day}
+                  className={classes(
+                    "w-3 h-3 rounded-sm",
+                    backgroundColor
+                  )}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-xs font-medium">{dateString}</p>
+                <p className="text-xs font-medium">Total: {value}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         )
+
       }
     }
 
     return squares;
-  }, [data, currentYear, adjustedStartMonth, currentMonth]);
+  }, [data, currentYear, adjustedStartMonth, currentMonth, today]);
 
   return (
     <div className="bg-accent rounded-lg grid grid-rows-[repeat(7,1fr)] justify-center grid-flow-col-dense gap-x-2.5 gap-y-2.5 p-4">
