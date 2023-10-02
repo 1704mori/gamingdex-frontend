ca77b2197268e344b6914ec1e1b598b76cf4b10e
mori1704 <1704mori@gmail.com>
2023-10-02 11:21:20 -0300

    fix api url env

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL ?? "http://localhost:3002";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3002";
 export const CDN_URL = process.env.CDN_URL ?? "http://";
 export const BASE_URL = process.env.BASE_URL ?? "http://192.168.0.7:3000";
